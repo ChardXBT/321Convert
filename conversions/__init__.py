84d2950a3546dc11480eb84356a3c33c1f425ed5
Eric Song <ericsuper8890@gmail.com>
2025-04-04 11:51:19 -0400

    Removed File modification code from HTML and CSS code, as well is init.py

--- conversions/__init__.py
+++ conversions/__init__.py
@@ -1,4 +1,3 @@
 # Import all converters to register them with the factory
 from conversions.image_converter import *
-from conversions.pdf_converter import *
 from conversions.document_converter import *
